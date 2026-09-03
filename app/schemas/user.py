b5fa794119e14413fb3b23c6c0845d72f61a32d2
vision <visionthapa8540@gmail.com>
2026-09-03 19:10:30 +0545

    feat: creation of user base

---
+++ app/schemas/user.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel,Field,UUID4,EmailStr,field_validator
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+import re
+
+class Role(str,Enum):
+    super_admin="SUPER_ADMIN"
+    prison_admin="PRISON_ADMIN"
+    customer="CUSTOMER"
+
+class UserBase(BaseModel):
+    email:EmailStr
+    full_name:str
+    role:Role
+
+    @field_validator("full_name")
+    @classmethod
+    def name(cls,value:str)->str:
+        value=" ".join(value.split())
+        if len(value) <3 or len(value) > 100:
+            raise ValueError("Full name must be between 3 to 100 characters")
+        parts=value.split()
+        if len(parts) < 2:
+            raise ValueError("Please provide both  first an last name")
+
+        if not re.match(r"^[A-Za-z\s'-]+$", value):
+            raise ValueError("Full name can only contain letters,spaces,hypness,and all unnessary attributes")
+
+        return value.title()
+
+        
+
+
+
+    
+
+
+
